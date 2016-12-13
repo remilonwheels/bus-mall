671052ad0898458a9653ec41a92986759d338782
Remil Marzan <remilonwheels@gmail.com>
2016-12-12 16:42:13 -0800

    completed constructor, and random image generator

--- app.js
+++ app.js
@@ -1,1 +1,93 @@
 'use strict';
+
+var allProducts = [];
+var productNames = ['bag', 'banana', 'boots', 'breakfast', 'bubblegum', 'chair'];
+var currentProductLeftIndex = -1;
+var currentProductCenterIndex = -1;
+var currentProductRightIndex = -1;
+
+var productSection = document.getElementById('product-section');
+var productLeft = document.getElementById('product-left');
+var productCenter = document.getElementById('product-center');
+var productRight = document.getElementById('product-right');
+
+function Product(productName) {
+  this.productName = productName;
+  this.path = 'img/' + productName + '.jpg';
+  this.tally = 0;
+  this.views = 0;
+
+  allProducts.push(this);
+}
+
+//Create Product Instances
+for (var i = 0; i < productNames.length; i++){
+  new Product(productNames[i]);
+}
+
+function populateProductSection() {
+  var productArray = generateUniqueArray(3);
+  var productLeftIndex = productArray[0];
+  var productCenterIndex = productArray[1];
+  var productRightIndex = productArray[2];
+
+  productLeft.src = allProducts[productLeftIndex].path;
+  productCenter.src = allProducts[productCenterIndex].path;
+  productRight.src = allProducts[productRightIndex].path;
+
+  currentProductLeftIndex = productLeftIndex;
+  currentProductCenterIndex = productCenterIndex;
+  currentProductRightIndex = productRightIndex;
+}
+
+function generateUniqueArray(arrayLength) {
+  var productSet = [];
+
+  for (var i = 0; i < arrayLength; i++) {
+    var randomIndex = generateRandomIndex();
+    while (productSet.indexOf(randomIndex) !== -1) {
+      randomIndex = generateRandomIndex();
+    }
+    productSet.push(randomIndex);
+  }
+
+  return productSet;
+}
+
+function generateRandomIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+function handleProductClick(event) {
+  event.preventDefault();
+  var selectedProductID = event.target.id;
+
+  if (selectedProductID === 'product-left') {
+    console.log('left-selected');
+    console.log(allProducts[currentProductLeftIndex].tally);
+  }
+
+  if (selectedProductID === 'product-left') {
+    console.log('left-selected');
+    allProducts[currentProductLeftIndex].tally++;
+    console.log(allProducts[currentProductLeftIndex].tally);
+  }
+
+  if (selectedProductID === 'product-left') {
+    console.log('left-selected');
+    allProducts[currentProductLeftIndex].tally++;
+    console.log(allProducts[currentProductLeftIndex].tally);
+  }
+
+
+
+  console.log(selectedProductID);
+
+
+  // populateProductSection();
+}
+
+productSection.addEventListener('click', handleProductClick);
+
+
+populateProductSection();
